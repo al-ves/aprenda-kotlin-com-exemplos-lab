83fe146905b633b99bcfaaec9261b81e6ff0fd2e
Alex Alves <88153275+al-ves@users.noreply.github.com>
2022-12-12 22:38:47 -0300

    Resolução do desafio de código.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,71 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BÁSICO, INTERMEDIÁRIO, AVANÇADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome: String, val email: String, val id: Int)
 
-class Usuario
+data class ConteudoEducacional(var nome: String, val duracao: Int = 0, val nivel: Nivel = Nivel.BÁSICO)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel = Nivel.BÁSICO) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Cria algumas instâncias de ConteudoEducacional
+    val cursoKotlin = ConteudoEducacional("Introdução ao Kotlin", 60)
+    val cursoJava = ConteudoEducacional("Introdução ao Java", 120)
+    val cursoPython = ConteudoEducacional("Introdução ao Python", 180)
+    
+    // Cria uma instância de Formacao
+    val formacao = Formacao("Programação", listOf(cursoKotlin, cursoJava, cursoPython))
+    
+    // Cria algumas instâncias de Usuario
+    val usuario1 = Usuario("João", "joao@email.com", 1)
+    val usuario2 = Usuario("Maria", "maria@email.com", 2)
+    val usuario3 = Usuario("José", "jose@email.com", 3)
+    
+    // Matricula os usuários na formação
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    formacao.matricular(usuario3)
+
+	// Testa a adição de usuários na lista de inscritos
+	println("Testando adição de usuários na lista de inscritos:")
+	println("Número de inscritos: ${formacao.inscritos.size}")
+	println()
+
+	// Testa a propriedade "nome" do objeto ConteudoEducacional
+	println("Testando propriedade 'nome' do objeto ConteudoEducacional:")
+	println("Nome do curso de Kotlin: ${cursoKotlin.nome}")
+	println()
+
+	// Testa a propriedade "duracao" do objeto ConteudoEducacional
+	println("Testando propriedade 'duracao' do objeto ConteudoEducacional:")
+	println("Duração do curso de Python: ${cursoPython.duracao} minutos")
+	println()
+
+	// Testa a propriedade "nivel" do objeto ConteudoEducacional
+	println("Testando propriedade 'nivel' do objeto ConteudoEducacional:")
+	println("Nível do curso de Java: ${cursoJava.nivel}")
+	println()
+
+	// Testa a propriedade "nome" do objeto Formacao
+	println("Testando propriedade 'nome' do objeto Formacao:")
+	println("Nome da formação: ${formacao.nome}")
+	println()
+
+	// Testa a propriedade "conteudos" do objeto Formacao
+	println("Testando propriedade 'conteudos' do objeto Formacao:")
+	println("Conteúdos da formação: ${formacao.conteudos.map { it.nome }}")
+	println()
+
+	// Testa a propriedade "nivel" do objeto Formacao
+	println("Testando propriedade 'nivel' do objeto Formacao:")
+	println("Nível da formação: ${formacao.nivel}")
+	println()
+
 }
+
